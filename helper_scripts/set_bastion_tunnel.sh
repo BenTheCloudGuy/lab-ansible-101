52c37d4189daee5c0734b5e9ee3612733ca09797
BenTheBuilder <47876371+BenTheCloudGuy@users.noreply.github.com>
2025-01-16 19:44:17 +0000

    fun fun

--- helper_scripts/set_bastion_tunnel.sh
+++ helper_scripts/set_bastion_tunnel.sh
@@ -19,7 +19,19 @@ WINVM02=${WINVM02,,}
 VM_RESOURCE_ID="/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GROUP}/providers/Microsoft.Compute/virtualMachines/${LINUXVM}"
 RESOURCE_PORT="22"
 LOCAL_PORT="10022"
-
-# Set up Bastion Tunnel to LINUXVM
 az network bastion tunnel --name "${BASTION_NAME}" --resource-group "${RESOURCE_GROUP}" --target-resource-id "${VM_RESOURCE_ID}" --resource-port "${RESOURCE_PORT}" --port "${LOCAL_PORT}"
 
+
+
+# WIN01 VM Configs
+VM_RESOURCE_ID="/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GROUP}/providers/Microsoft.Compute/virtualMachines/${WINVM01}"
+RESOURCE_PORT="5986"
+LOCAL_PORT="15986"
+az network bastion tunnel --name "${BASTION_NAME}" --resource-group "${RESOURCE_GROUP}" --target-resource-id "${VM_RESOURCE_ID}" --resource-port "${RESOURCE_PORT}" --port "${LOCAL_PORT}"
+
+
+# WIN02 VM Configs
+VM_RESOURCE_ID="/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GROUP}/providers/Microsoft.Compute/virtualMachines/${WINVM02}"
+RESOURCE_PORT="5986"
+LOCAL_PORT="25986"
+az network bastion tunnel --name "${BASTION_NAME}" --resource-group "${RESOURCE_GROUP}" --target-resource-id "${VM_RESOURCE_ID}" --resource-port "${RESOURCE_PORT}" --port "${LOCAL_PORT}"
